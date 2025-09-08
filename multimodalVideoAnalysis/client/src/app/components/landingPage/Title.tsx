f6c0054a048b48051e645a050226c222e62fa4ba
Maamoon Salman <137584309+maamoonzsalman@users.noreply.github.com>
2025-09-08 12:34:32 -0700

    feat: initialize Next.js app and add landing page

---
+++ multimodalVideoAnalysis/client/src/app/components/landingPage/Title.tsx
@@ -0,0 +1,9 @@
+export default function Title() {
+    return (
+        <div className="bg-purple-900 h-24 flex flex-col justify-center">
+            <div className='text-2xl text-blue-400'>
+                Multimodal Video Analysis System
+            </div>
+        </div>
+    )
+} 
